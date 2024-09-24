7c3f100d4c4d9a80aafeaa1295f90891e95307eb
Kwon <141651704+ThiagoKwon@users.noreply.github.com>
2024-09-24 19:30:41 -0300

    ambiente eclipse
    eclipse

---
+++ TDE_2/src/com/code/morse/Main.java
@@ -0,0 +1,20 @@
+package com.code.morse;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Tree arv = new Tree();
+		String[] alfabeto = { "E", "T", "I", "A", "N", "M", "S", "U", "R", "W", "D", "K", "G", "O", "H", "V", "F", "L",
+				"P", "J", "B", "X", "C", "Y", "Z", "Q" };
+		String[] morse = { ".", "-", "..", ".-", "-.", "--", "...", "..-", ".-.", ".--", "-..", "-.-", "--.", "---",
+				"....", "...-", "..-.", ".-..", ".--.", ".---", "-...", "-..-", "-.-.", "-.--", "--..", "--.-" };
+
+		for (int i = 0; i < alfabeto.length; i++) {
+			arv.add(alfabeto[i], morse[i]);
+		}
+
+		System.out.println(arv.searchWord("-/..../../.-/--./---"));
+	}
+
+}
---
+++ TDE_2/src/com/code/morse/Tree.java
@@ -0,0 +1,60 @@
+package com.code.morse;
+
+public class Tree {
+	private Node<String> root = new Node<String>("começo");
+	private String result = "PALAVRA: ";
+
+	public Node<String> getRoot() {
+		return root;
+	}
+
+	// ADICIONAR NOVO VALOR
+	public void add(String value, String instruc) {
+		Node<String> newNode = new Node<String>(value);
+
+		Node<String> current = this.root;
+
+		for (int i = 0; i < instruc.length(); i++) {
+			String carac = String.valueOf(instruc.charAt(i));
+			if (carac.equals(".")) {
+				if (i == instruc.length() - 1 && current.getLeft() == null) {
+					current.setLeft(newNode);
+				} else {
+					current = current.getLeft();
+				}
+			} else if (carac.equals("-")) {
+				if (i == instruc.length() - 1 && current.getRight() == null) {
+					current.setRight(newNode);
+				} else {
+					current = current.getRight();
+				}
+			}
+		}
+	}
+
+	public String searchWord(String instruc) {
+		Node<String> current = this.root;
+		instruc = instruc + "/";
+
+		for (int i = 0; i < instruc.length(); i++) {
+			String carac = String.valueOf(instruc.charAt(i));
+			if (carac.equals(".")) {
+				current = current.getLeft();
+			} else if (carac.equals("-")) {
+				current = current.getRight();
+			} else {
+				setResult(getResult() + current.getValue());
+				current = this.root;
+			}
+		}
+		return getResult();
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+}

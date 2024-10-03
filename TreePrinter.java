d625d57a7d93f2fc296be879a183cf1554fa7231
Pedro Fauth <fauthpedroa@gmail.com>
2024-10-03 20:20:36 -0300

    retira comentarios

--- TreePrinter.java
+++ TreePrinter.java
@@ -2,30 +2,28 @@ package com.code.morse;
 
 public class TreePrinter {
 
-    // Função auxiliar que imprime a árvore binária em formato de "bolinhas"
+
     public static void printTree(Node<String> root, int level) {
         if (root == null) {
             return;
         }
 
-        // Imprime a árvore da direita para a esquerda, para que a esquerda fique "para baixo"
+
         printTree(root.getRight(), level + 1);
 
-        // Imprime os espaços para simular o nível da árvore
+
         if (level != 0) {
             for (int i = 0; i < level - 1; i++) {
-                System.out.print("|\t");  // Espaço entre níveis
+                System.out.print("|\t");
             }
-            System.out.println("|------- " + root.getValue());  // Nó atual
+            System.out.println("|------- " + root.getValue());
         } else {
-            System.out.println(root.getValue());  // Raiz da árvore
+            System.out.println(root.getValue());
         }
 
-        // Imprime o nó da esquerda
         printTree(root.getLeft(), level + 1);
     }
 
-    // Método público para chamar a função de impressão
     public static void showTree(Node<String> root) {
         printTree(root, 0);  // Começa da raiz (nível 0)
     }
